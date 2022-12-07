47960dd245f16258ffe10c6150207d60df848771
vlad2021-pro <84739712+vlad2021-pro@users.noreply.github.com>
2022-12-07 13:37:16 +0100

    message

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
-import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import './index.css';
+import App from './App';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
+    
     <App />
+    
   </React.StrictMode>
 );
+
+
